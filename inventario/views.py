b2e3c76bc45f64790c4b8f080dc10aab84387660
maxwelljuni6-stack <maxwelljuni6@gmail.com>
2026-04-27 22:36:22 -0400

    cambios en el view

--- inventario/views.py
+++ inventario/views.py
@@ -3,17 +3,18 @@ from django.contrib.auth.decorators import login_required
 from .models import Producto 
 from .forms import ProductoForm 
 
-
 def lista_productos(request): 
     productos = Producto.objects.all() 
-return render(request, 'inventario/lista.html', {'productos': productos}) 
+    return render(request, 'inventario/lista.html', {'productos': productos}) 
+
 @login_required  # Protección nativa (Pág. 9 )
 def crear_producto(request): 
- if request.method == 'POST': 
+    if request.method == 'POST': 
         form = ProductoForm(request.POST) 
-if form.is_valid(): 
+        if form.is_valid(): 
             form.save() 
-return redirect('lista_productos') 
-else: 
+            return redirect('lista_productos')  
+
+    else: 
         form = ProductoForm() 
-return render(request, 'inventario/formulario.html', {'form': form})
+        return render(request, 'inventario/formulario.html', {'form': form}) 
